23e2424f7363ac53d3976c0d9cbb348da51ea612
Malgorzata Zawisza <milele.fushi@gmail.com>
2018-05-07 21:16:47 +0200

    Begin creating login page and admin page

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -344,3 +344,12 @@ function confirmOrder(){
 }
 
 document.getElementById('buttonConfirm').addEventListener('click', confirmOrder);
+
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
